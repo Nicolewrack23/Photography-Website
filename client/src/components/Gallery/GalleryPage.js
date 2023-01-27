3df37e66f41a01cd5fa7e698d2fad7a7dfccaf2c
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-01-27 19:11:03 +1300

    added images in will change when images are found

--- client/src/components/Gallery/GalleryPage.js
+++ client/src/components/Gallery/GalleryPage.js
@@ -1,10 +1,19 @@
 import styles from "./GalleryPage.module.css";
+import image from "./imageplaceholder.png";
 
 const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
-        <h1>Gallery</h1>
+        <h1 className={styles.galleryH1}>Gallery</h1>
+        <div className={styles.images}>
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+        </div>
       </div>
     </>
   );
