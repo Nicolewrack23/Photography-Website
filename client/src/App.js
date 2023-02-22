7dd3beab7fdf8a449bbd32626c0ca7b7d9247670
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-02-23 12:47:24 +1300

    adding page to check the menu is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import SideNavigation from "./components/Navigation/SideNavigation";
+import GalleryPage from "./components/Gallery/GalleryPage";
 
 function App() {
   return (
     <div>
       <SideNavigation />
+      <GalleryPage />
     </div>
   );
 }
