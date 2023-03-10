a729d82d5d94d7c1208441952aff84dbf062b7e9
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-03-10 13:37:35 +1300

    changed image, removed h1 as unneeded add 3 more imgs

--- client/src/components/Gallery/GalleryPage.js
+++ client/src/components/Gallery/GalleryPage.js
@@ -1,12 +1,14 @@
 import styles from "./GalleryPage.module.css";
-import image from "./imageplaceholder.png";
+import image from "./beach.jpeg";
 
 const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
-        <h1 className={styles.galleryH1}>Gallery</h1>
         <div className={styles.images}>
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
+          <img className={styles.galleryImages} src={image} alt="placeholder" />
           <img className={styles.galleryImages} src={image} alt="placeholder" />
           <img className={styles.galleryImages} src={image} alt="placeholder" />
           <img className={styles.galleryImages} src={image} alt="placeholder" />
