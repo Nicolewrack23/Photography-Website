1fd6aff4cdb4215c566fb47ff2430855c44a812c
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-03-17 12:36:40 +1300

    creating an about me card

--- client/src/components/About/AboutPage.js
+++ client/src/components/About/AboutPage.js
@@ -1,20 +1,28 @@
 import styles from "./AboutPage.module.css";
 import image from "../Photos/photo.jpeg";
+import { FaInstagram, FaFacebookF } from "react-icons/fa";
 
 const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.welcomeComponents}>
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
-          <p className={styles.welcomeH1}>Hi there, I am</p>
-          <h1 className={styles.welcomeH1}>Nicole Wrack</h1>
-          <p className={styles.welcomeH1}>Software Developer</p>
-          <p className={styles.welcomeP}>
-            A quick bit about my self, The quick brown fox jumps over the lazy
-            dogs The quick brown fox jumped over the lazy dogs
-          </p>
-          <button>Click Me</button>
+        <div className={styles.card}>
+          <img className={styles.cardImage} src={image} alt="placeholder" />
+
+          <div className={styles.textStyling}>
+            <p>Hi there, I am</p>
+            <h1 className={styles.heading}>Nicole Wrack</h1>
+            <p>Software Developer</p>
+            <p>
+              A quick bit about my self, The quick brown fox jumps over the lazy
+              dogs The quick brown fox jumped over the lazy dogs
+            </p>
+            <button>Click Me</button>
+            <div>
+              <FaInstagram className={styles.icons} />
+              <FaFacebookF className={styles.icons} />
+            </div>
+          </div>
         </div>
       </div>
     </>
