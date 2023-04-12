ac26c2ad466797d2b4f4cb597c2d4888f1ed016f
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-04-13 11:09:00 +1200

    adding styling to elements

--- client/src/components/About/AboutPage.js
+++ client/src/components/About/AboutPage.js
@@ -1,24 +1,26 @@
 import styles from "./AboutPage.module.css";
-import Image from "../Photos/photo.jpeg";
+import Image from "../Gallery/beach.jpeg";
 
 const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.AboutTitle}>
-          <p className={styles.aboutWho}>WHO AM I</p>
+          <p className={styles.aboutWho}>
+            <strong>WHO AM I</strong>
+          </p>
           <h1 className={styles.aboutH1}>About Me</h1>
         </div>
         <div className={styles.aboutGrid}>
           <div className={styles.aboutImage}>
             <img className={styles.cardImage} src={Image} alt="placeholder" />
           </div>
-          <div>
-            <p>
+          <div className={styles.aboutText}>
+            <p className={styles.textHighlight}>
               I am a fitness trainer who's passionate about helping people
               achieve their health and fitness goals.
             </p>
-            <p>
+            <p className={styles.textLow}>
               I've worked with students and professionals over the past seven
               years. Weight training and cardio workouts are my main areas of
               expertise.
