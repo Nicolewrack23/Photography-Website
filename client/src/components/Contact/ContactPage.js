f6b417b0466df633b80e0a12df6b8a43b9dec492
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-04-14 12:27:50 +1200

    adding styling for sementics

--- client/src/components/Contact/ContactPage.js
+++ client/src/components/Contact/ContactPage.js
@@ -7,15 +7,24 @@ const EmailForm = () => {
   const [message] = useState("");
 
   return (
-    <form>
-      <div className={styles.contactGrid}>
-        <label>Name:</label>
-        <input type="text" value={name} />
-        <label>Email:</label>
-        <input type="email" value={email} />
-        <label>Message:</label>
-        <textarea value={message} />
-        <button type="submit">Send</button>
+    <form className={styles.form}>
+      <div className={styles.contactCard}>
+        <div className={styles.contactGrid}>
+          <div>
+            <h1>Contact Me </h1>
+            <p>
+              Let's connect! Fill out the contact form below, and I'll be happy
+              to discuss availability and answer any questions you have.
+            </p>
+          </div>
+          <label>Name:</label>
+          <input type="text" value={name} />
+          <label>Email:</label>
+          <input type="email" value={email} />
+          <label>Message:</label>
+          <textarea value={message} />
+          <button type="submit">Send</button>
+        </div>
       </div>
     </form>
   );
