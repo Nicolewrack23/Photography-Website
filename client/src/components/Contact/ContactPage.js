3579cad5cc9222533b74af439ff5b1e0d66ce84e
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-04-14 13:16:43 +1200

    styling contact page

--- client/src/components/Contact/ContactPage.js
+++ client/src/components/Contact/ContactPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Image from "../Gallery/beach.jpeg";
 import styles from "./ContactPage.module.css";
 
 const EmailForm = () => {
@@ -9,21 +10,42 @@ const EmailForm = () => {
   return (
     <form className={styles.form}>
       <div className={styles.contactCard}>
-        <div className={styles.contactGrid}>
-          <div>
-            <h1>Contact Me </h1>
-            <p>
-              Let's connect! Fill out the contact form below, and I'll be happy
-              to discuss availability and answer any questions you have.
-            </p>
+        <div className={styles.contactBox}>
+          <div className={styles.aboutImage}>
+            <img className={styles.cardImage} src={Image} alt="placeholder" />
           </div>
-          <label>Name:</label>
-          <input type="text" value={name} />
-          <label>Email:</label>
-          <input type="email" value={email} />
-          <label>Message:</label>
-          <textarea value={message} />
-          <button type="submit">Send</button>
+          <div className={styles.contactGrid}>
+            <div>
+              <h1 className={styles.contactTitle}>Contact Me </h1>
+              <p className={styles.contactSubTitle}>
+                Let's connect! Fill out the contact form below, and I'll be
+                happy to discuss availability and answer any questions you have.
+              </p>
+            </div>
+            <label>Name:</label>
+            <input
+              className={styles.contactInput}
+              placeholder="Name"
+              type="text"
+              value={name}
+            />
+            <label>Email:</label>
+            <input
+              className={styles.contactInput}
+              placeholder="Email address"
+              type="email"
+              value={email}
+            />
+            <label>Message:</label>
+            <textarea
+              className={`${styles.contactInput}, ${styles.contactInputMessage}`}
+              placeholder="Your message"
+              value={message}
+            />
+            <button className={styles.contactButton} type="submit">
+              Send
+            </button>
+          </div>
         </div>
       </div>
     </form>
