ac0fbe1e469b1c0b70ae04347fec758771c69b9f
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-05-05 17:03:23 +1200

    fixed layout for the mobile and 40rem

--- client/src/components/About/AboutPage.js
+++ client/src/components/About/AboutPage.js
@@ -1,28 +1,29 @@
 import styles from "./AboutPage.module.css";
-// import Image from "../Gallery/beach.jpeg";
+import camera from "../Photos/camera.svg";
+import Image from "../Photos/photo.jpeg";
 
 const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.AboutTitle}>
-          <h1 className={styles.aboutH1}>About Me</h1>
-        </div>
-        <div className={styles.aboutGrid}>
-          {/* <div className={styles.aboutImage}>
-            <img className={styles.cardImage} src={Image} alt="placeholder" />
-          </div> */}
-          <div className={styles.aboutText}>
-            <p className={styles.textHighlight}>
-              I am a fitness trainer who's passionate about helping people
-              achieve their health and fitness goals.
-            </p>
-            <p className={styles.textLow}>
-              I've worked with students and professionals over the past seven
-              years. Weight training and cardio workouts are my main areas of
-              expertise.
-            </p>
+        <div className={styles.textPortion}>
+          <div className={styles.heading}>
+            <img className={styles.camera} src={camera} alt="camera" />
+            <h1 className={styles.aboutH1}>About Me</h1>
           </div>
+          <p className={styles.textHighlight}>
+            I am a fitness trainer who's passionate about helping people achieve
+            their health and fitness goals.
+          </p>
+          <p className={styles.textLow}>
+            I've worked with students and professionals over the past seven
+            years. Weight training and cardio workouts are my main areas of
+            expertise.
+          </p>
+          <button className={styles.button}>CONTACT ME</button>
+        </div>
+        <div className={styles.aboutImage}>
+          <img className={styles.image} src={Image} alt="placeholder" />
         </div>
       </div>
     </>
