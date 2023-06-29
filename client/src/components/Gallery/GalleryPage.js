51314d0019f25377d3a0fbe74ec9b8cf066cb414
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-06-29 14:11:36 +1200

    adding for smaller views

--- client/src/components/Gallery/GalleryPage.js
+++ client/src/components/Gallery/GalleryPage.js
@@ -6,32 +6,63 @@ const WelcomePage = () => {
   return (
     <>
       <div className={styles.container}>
-        <div className={styles.images}>
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
-          <img
-            className={styles.galleryImages}
-            src={skyImage}
-            alt="placeholder"
-          />
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
-          <img
-            className={styles.galleryImages}
-            src={skyImage}
-            alt="placeholder"
-          />
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
-          <img
-            className={styles.galleryImages}
-            src={skyImage}
-            alt="placeholder"
-          />
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
-          <img
-            className={styles.galleryImages}
-            src={skyImage}
-            alt="placeholder"
-          />
-          <img className={styles.galleryImages} src={image} alt="placeholder" />
+        <div className={styles.galleryImagesContainer}>
+          <div className={styles.images}>
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={skyImage}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={skyImage}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={skyImage}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={skyImage}
+              alt="placeholder"
+            />
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+          </div>
+        </div>
+        <div className={styles.mobileGalleryImageContainer}>
+          <div className={styles.mobileImages}>
+            <img
+              className={styles.galleryImages}
+              src={image}
+              alt="placeholder"
+            />
+          </div>
         </div>
       </div>
     </>
