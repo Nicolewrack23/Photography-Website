322fcea52111a9e582a4fe705ba0263b308dd943
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-06-29 14:09:32 +1200

    moved into side nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,9 @@
-import styles from "./App.module.css";
 import SideNavigation from "./components/Navigation/SideNavigation";
-import GalleryPage from "./components/Gallery/GalleryPage";
-import AboutPage from "./components/About/AboutPage";
-import ContactPage from "./components/Contact/ContactPage";
 
 function App() {
   return (
     <div>
       <SideNavigation />
-      <GalleryPage />
-      <div className={styles.gradient}>
-        <AboutPage />
-        <ContactPage />
-      </div>
     </div>
   );
 }
