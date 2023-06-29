159b0e1052e645f690e4c3224318b92d0708ba51
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-06-29 14:12:30 +1200

    adding landing page

--- client/src/components/Welcome/WelcomePage.js
+++ client/src/components/Welcome/WelcomePage.js
@@ -1,19 +1,31 @@
+import React from "react";
 import styles from "./WelcomePage.module.css";
 
 const WelcomePage = () => {
+  const scrollToGallery = () => {
+    const gallerySection = document.getElementById("gallery");
+    if (gallerySection) {
+      gallerySection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
-    <>
-      <div className={styles.container}>
-        <div className={styles.welcomeComponents}>
-          <h1 className={styles.welcomeH1}>Welcome</h1>
-          <p className={styles.welcomeP}>
-            The quick brown fox jumps over the lazy dogs The quick brown fox
-            jumped over the lazy dogs
-          </p>
-          <button>Click Me</button>
+    <div className={styles.container}>
+      <div className={styles.welcomeComponents}>
+        <h1 className={styles.welcomeH1}>Welcome to the Photography Website</h1>
+        <p className={styles.welcomeP}>
+          Explore the captivating world of photography captured through our
+          lens. Witness breathtaking moments frozen in time.
+        </p>
+
+        <div className={styles.buttonContainer}>
+          <button className={styles.button} onClick={scrollToGallery}>
+            View Gallery
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
+
 export default WelcomePage;
