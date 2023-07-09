adfcaeb0e92a8434b601558c82bf609eaaf3cac9
Nicole Wrack <nicole.wrack@developersinstitute.co.nz>
2023-07-09 20:15:17 +1200

    react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SideNavigation from "./components/Navigation/SideNavigation";
+import PolicyPage from "./components/Policies/PolicyPage";
+import Disclaimer from "./components/Policies/Disclaimer";
+import Terms from "./components/Policies/Term";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <SideNavigation />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SideNavigation />} />
+        <Route path="/privacyPolicy" element={<PolicyPage />} />
+        <Route path="/disclaimer" element={<Disclaimer />} />
+        <Route path="/terms" element={<Terms />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
